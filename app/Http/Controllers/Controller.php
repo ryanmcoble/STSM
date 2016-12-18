9a76d361d566612fb00c80d2573d41e4d597b5db
Ryan Coble <ryan.m.coble@gmail.com>
2016-12-18 07:34:50 -0500

    Latest.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -8,7 +8,33 @@ use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesResources;
 
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\View;
+
+use ThemeSettingBuilder\ShopifyConnector;
+use ThemeSettingBuilder\ShopifyData;
+use App\Shop;
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, AuthorizesResources, DispatchesJobs, ValidatesRequests;
+
+    protected $shop;
+    protected $shopifyData;
+
+    public function __construct() {
+
+    	$this->shop = Shop::where('permanent_domain', Session::get('shop'))->first();
+
+        if($this->shop) {
+            $shopifyConnector = ShopifyConnector::connect($this->shop);
+            $this->shopifyData = new ShopifyData($shopifyConnector);
+
+            $themes = $this->shopifyData->getThemes();
+
+            View::share('shop', $this->shop);
+            View::share('api_key', env('API_KEY'));
+            View::share('themes', $themes);
+        }
+    }
 }

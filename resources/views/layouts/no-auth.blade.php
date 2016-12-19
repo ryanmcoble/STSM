2c1b391846bdad676b6542b575f76937a05161ff
Ryan Coble <ryan.m.coble@gmail.com>
2016-12-19 05:55:45 -0500

    Trying to get things working properly

--- resources/views/layouts/no-auth.blade.php
+++ resources/views/layouts/no-auth.blade.php
@@ -7,12 +7,12 @@
 
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 
-        <link rel="stylesheet" href="{{ asset('assets/css/normalize.css') }}">
-        <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+        <link rel="stylesheet" href="{{ secure_asset('assets/css/normalize.css') }}">
+        <link rel="stylesheet" href="{{ secure_asset('assets/css/bootstrap.min.css') }}" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
         
-        <link rel="stylesheet" href="{{ asset('assets/css/font-awesome.min.css') }}">
+        <link rel="stylesheet" href="{{ secure_asset('assets/css/font-awesome.min.css') }}">
 
-        <link rel="stylesheet" href="{{ asset('assets/css/main.css') }}">
+        <link rel="stylesheet" href="{{ secure_asset('assets/css/main.css') }}">
     </head>
     <body class="{{ $body_class }}">
 

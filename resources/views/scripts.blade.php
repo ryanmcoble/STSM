9b77f23abdbb142be19ea7349f1797661f0a9395
Ryan Coble <ryan.m.coble@gmail.com>
2016-12-19 05:52:45 -0500

    Trying to get things working properly

--- resources/views/scripts.blade.php
+++ resources/views/scripts.blade.php
@@ -3,6 +3,6 @@
 <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
 
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
-<script src="{{ asset('assets/js/jquery-simplecolorpicker.js') }}"></script>
+<script src="{{ secure_asset('assets/js/jquery-simplecolorpicker.js') }}"></script>
 
-<script src="{{ asset('assets/js/main.js') }}"></script>
+<script src="{{ secure_asset('assets/js/main.js') }}"></script>
